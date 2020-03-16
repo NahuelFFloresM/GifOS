9565ad145b0d37793b663a68fca87ae75012a4da
Sqall <arcangel6191@gmail.com>
2020-03-16 11:06:54 -0300

    Prueba captura gif

--- js/user.js
+++ js/user.js
@@ -7,10 +7,42 @@ function loadUserGifs(user){
     // });
 };
 
+let hours = `00`,minutes = `00`,seconds = `00`;
+let cronometerTag = document.getElementById('gif-cronometer');
+
+function chronometer() {
+    seconds ++
+    if (seconds < 10) seconds = `0` + seconds
+    if (seconds > 59) {
+        seconds = `00`
+        minutes ++
+        if (minutes < 10) minutes = `0` + minutes
+    }
+    if (minutes > 59) {
+      minutes = `00`
+      hours ++      
+      if (hours < 10) hours = `0` + hours
+    }
+    cronometerTag.textContent = `${hours}:${minutes}:${seconds}`
+  }
+
 document.getElementById('cancel-new-gif').addEventListener('click',function(){
     document.getElementById('capture_1').style.display = 'none';
     localStorage.removeItem('newGif-command');
 });
+
+document.getElementById('start-new-gif').addEventListener('click',function(){
+    document.getElementById('capture_1').style.display = 'none';
+    document.getElementById('capture_2').style.display = 'block';
+});
+
+document.getElementById('camera-container').addEventListener('click',function(){
+    document.getElementById('camera-container').style.display = 'none';
+    document.getElementById('record-container').style.display = 'inherit';
+    cronometerTag.style.display = 'block';
+    chronometerCall = setInterval(chronometer, 1000);
+});
+
 
 document.getElementById('btn-newGif').addEventListener('click',function(){
     document.getElementById('capture_1').style.display = 'block';
--- js/video.js
+++ js/video.js
@@ -2,17 +2,29 @@
 var constraints = { audio: true, video: { width: 1280, height: 720 } };
 let video = document.getElementById('gif-video');
 
-// recorder = RecordRTC(stream, {
-//     type: 'gif',
-//     frameRate: 1,
-//     quality: 10,
-//     width: 360,
-//     hidden: 240,    
-//     onGifRecordingStarted: function() {
-//         console.log('started')
-//     },
-// }
-// );
+function getStreamAndRecord () {
+    navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+        height: { max: 424 },
+        width: {max: 832 }
+    }}).then(function(stream) {
+        video.srcObject = stream;
+        video.play()
+    })
+}
+
+recorder = RecordRTC(video, {
+    type: 'gif',
+    frameRate: 1,
+    quality: 10,
+    width: 360,
+    hidden: 240,    
+    onGifRecordingStarted: function() {
+        console.log('started')
+    },
+}
+);
 
 // navigator.mediaDevices.getUserMedia(constraints)
 // .then(function(mediaStream) {
@@ -24,17 +36,6 @@ let video = document.getElementById('gif-video');
 // })
 // .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
 
-
-function getStreamAndRecord () {
-    navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-        height: { max: 480 }
-    }}).then(function(stream) {
-        video.srcObject = stream;
-        video.play()
-    })
-}
 
 // videotag.src = getStreamAndRecord();
 // videotag.play();
