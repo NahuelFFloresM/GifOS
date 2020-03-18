30c53c85e3b78876605a689ea35ebed745e0ad55
Sqall <arcangel6191@gmail.com>
2020-03-18 14:25:46 -0300

    Terminando CSS y cambio de imagenes. Crear gif en standby

--- js/video.js
+++ js/video.js
@@ -1,18 +1,28 @@
 // Prefer camera resolution nearest to 1280x720.
 var constraints = { audio: true, video: { width: 1280, height: 720 } };
 let video = document.getElementById('gif-video');
+var recorder;
 
-function getStream () {
+function getStreamAndRecord () {
     navigator.mediaDevices.getUserMedia({
     audio: false,
     video: {
         height: { max: 424 },
         width: {max: 832 }
     }}).then(function(stream) {
-        console.log(stream);
         video.srcObject = stream;
-        video.onloadedmetadata = function(e) {
-            video.play();
-          };
+        video.play()
     })
 }
+
+recorder = RecordRTC(video, {
+    type: 'gif',
+    frameRate: 1,
+    quality: 10,
+    width: 832,
+    height: 424,
+    hidden: 240,
+    onGifRecordingStarted: function() {
+        console.log('started')
+    },
+});
